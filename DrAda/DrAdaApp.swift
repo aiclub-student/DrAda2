ec32debf77faead866b4378fadbfbcab2a518c72
Amit Gupta <streamlit@gprof.com>
2022-04-09 17:18:43 -0700

    Initial Commit

---
+++ DrAda/DrAdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrAdaApp.swift
+//  DrAda
+//
+//  Created by Amit Gupta on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct DrAdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
